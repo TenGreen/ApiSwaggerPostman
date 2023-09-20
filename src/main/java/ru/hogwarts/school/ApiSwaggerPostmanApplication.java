fa318f07f1d779583d7d332b5996b2b91510efe0
TenGreen <grishintut@yandex.ru>
2023-09-20 23:13:55 +0300

    не стал создавать метод поиска по ID в репозитории студента, использовал метод по умолчанию. Код запустился. На проверку.

--- src/main/java/ru/hogwarts/school/ApiSwaggerPostmanApplication.java
+++ src/main/java/ru/hogwarts/school/ApiSwaggerPostmanApplication.java
@@ -12,7 +12,6 @@ public class ApiSwaggerPostmanApplication {
 
 		SpringApplication.run(ApiSwaggerPostmanApplication.class, args);
 
-
 	}
 
 }
--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.Student;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
@@ -15,6 +14,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findAllByAgeBetween(int min, int max);
 
     List<Student> findAllByFaculty_Id(long facultyId);
-    Optional<Student> findAllByStudent_Id(long studentId);
+//    Optional<Student> findByStudent_Id(long studentId);
 
 }
